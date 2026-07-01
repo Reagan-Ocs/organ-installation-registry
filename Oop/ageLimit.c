79ccedb81c30a918a95d0bf73db62f9ab51de83b
Reagan Isaac <224869571+Reagan-Ocs@users.noreply.github.com>
2026-07-01 12:34:06 +0300

    Adding the patient prompts

---
+++ Oop/ageLimit.c
@@ -0,0 +1,34 @@
+/*Design and implement a command-line utility in C that calculates and
+tracks the expiration dates of patients using high-performance cybernetic
+implants. Because these implants put extreme strain on the human nervous system,
+they have a strict, hard-coded 13-year operational limit. The program will calculate
+the user's terminal year, determine the exact time remaining relative to the current year
+(2026), and generate a diagnostic biological status report.*/
+#include <stdio.h>
+char patientName[100];
+int yearInstalled;
+int expirationYear;
+int currentYear;
+int timeRemaining;
+char bioStatus;
+int main()
+{
+
+    printf("== = CYBERNETICS DIAGNOSTICS == =\n");
+    printf("Scanning host integration data...\n");
+    printf("Enter patient's name:");
+    scanf("%s", patientName);
+    printf("Enter your year of implant installation: \n");
+    scanf("%d", &yearInstalled);
+    printf("Enter the current year: \n");
+    scanf("%d", &currentYear);
+    printf("-- -DIAGNOSTIC REPORT : %s-- -\n", patientName);
+    printf("                       Installation Year: %d Expiration Year: %d\n", yearInstalled, expirationYear);
+
+    printf("                       System Timeline: %d years remaining.\n", timeRemaining);
+    printf("                       Biostatus Check: %c \n", bioStatus);
+    printf("                       Micro-cellular degradation detected.Mild biological fatigue.-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --");
+
+    timeRemaining = expirationYear - currentYear;
+    return 0;
+}
